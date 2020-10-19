d33a6a4a927e0b58aca60b2da11ef3ac9fd26bf5
林邦泉 <linbq@techsure.com.cn>
2020-10-19 13:11:34 +0800

    增加事务及事务回滚

--- src/test/java/codedriver/BaseTest.java
+++ src/test/java/codedriver/BaseTest.java
@@ -1,10 +1,12 @@
 package codedriver;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 
@@ -16,16 +18,20 @@ import codedriver.framework.asynchronization.threadlocal.TenantContext;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-context.xml")
+@Transactional(transactionManager = "transactionManager")//开启事务
+@Rollback(value = true)//事务回滚，数据不会插入到数据库
 public class BaseTest {
-
-    @Before
-    public void before() {
+    
+    @BeforeClass
+    public static void beforeClass() {
         /** 默认租户为techsure，默认查询codedriver_techsure库，如需查询codedriver库，执行这条语句切换TenantContext.get().setUseDefaultDatasource(true); **/
         TenantContext.init("techsure");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
     
-    @After
-    public void after() {
+    @AfterClass
+    public static void afterClass() {
         TenantContext.get().release();
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
 }
