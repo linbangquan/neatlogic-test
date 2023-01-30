8bc8ee2a9ed754360ea4753d96b5a7b69252a99f
zenkiray <zenkiray@live.com>
2023-01-30 16:44:27 +0800

    test
    Signed-off-by: zenkiray <zenkiray@live.com>

--- src/test/java/codedriver/BaseTest.java
+++ src/test/java/codedriver/BaseTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver;
 
 import org.junit.AfterClass;
@@ -10,24 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 
-/**
- * 
-* @Time:2020年10月14日
-* @ClassName: BaseTest 
-* @Description: 测试基类，所有测试类都继承这个类
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-context.xml")
 @Transactional(transactionManager = "transactionManager")//开启事务
 @Rollback(value = true)//事务回滚，数据不会插入到数据库
 public class BaseTest {
-    
+
     @BeforeClass
     public static void beforeClass() {
         /** 默认租户为techsure，默认查询codedriver_techsure库，如需查询codedriver库，执行这条语句切换TenantContext.get().setUseDefaultDatasource(true); **/
-        TenantContext.init("techsure");
+        TenantContext.init("develop");
     }
-    
+
     @AfterClass
     public static void afterClass() {
         TenantContext.get().release();
